92fa503be67e589492f1dcc3126a7f6c7e763cdd
https-allan <code.allanjose@gmail.com>
2024-01-02 01:26:23 -0400

    Update de modelo

--- Seção-3/aula25/aula25.js
+++ Seção-3/aula25/aula25.js
@@ -1,5 +1,7 @@
 const numero = 5;
 
 if(numero >= 0 && numero <= 5) {
-    console.log('O numero está entre 0 e 5');
-} 
+    console.log('O numero está entre 0 e 5.');
+} else {
+    console.log('O número NÃO está entre 0 e 0.');
+}
