a4e833cd4e88cc5b3ec1d88c5fcdea33a8a6d698
https-allan <code.allanjose@gmail.com>
2024-01-19 04:09:24 -0400

    Update

--- Secao-3/aula35/assets/src/js/main.js
+++ Secao-3/aula35/assets/src/js/main.js
@@ -1,23 +1,21 @@
-addEventListener('DOMContentLoaded', function () {
-    const elementos = [
-        { tag: 'p', texto: 'Exercício Executado com sucesso!' },
-        { tag: 'div', texto: 'Fiz o exercício usando JavaScript para criar as tags.' },
-        { tag: 'footer', texto: 'Aprender sempre é bom!' },
-        { tag: 'section', texto: 'Estude não seja um procrastinador!' },
-    ];
+const elementos = [
+    { tag: 'p', texto: 'Exercício Executado com sucesso!' },
+    { tag: 'div', texto: 'Fiz o exercício usando JavaScript para criar as tags.' },
+    { tag: 'footer', texto: 'Aprender sempre é bom!' },
+    { tag: 'section', texto: 'Estude não seja um procrastinador!' },
+];
 
-    const container = document.querySelector('.container');
+const container = document.querySelector('.container');
 
-    const div = document.createElement('div');
+const div = document.createElement('div');
 
-    for (let i = 0; i < elementos.length; i++) {
-        const { tag, texto } = elementos[i];
-        const criarTag = document.createElement(tag);
+for (let i=0; i < elementos.length; i++) {
+    const { tag, texto } = elementos[i];
+    const criarTag = document.createElement(tag);
 
-        criarTag.innerHTML = texto;
+    criarTag.innerText = texto;
 
-        div.appendChild(criarTag);
-    }
+    div.appendChild(criarTag);
+}
 
-    container.appendChild(div);
-})
+container.appendChild(div);
