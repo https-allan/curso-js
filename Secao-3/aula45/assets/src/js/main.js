b5045603d2e1bb31f7fc47ee7fb57998eb0d1f32
https-allan <code.allanjose@gmail.com>
2024-04-01 20:50:52 -0400

    Update

--- Secao-3/aula45/assets/src/js/main.js
+++ Secao-3/aula45/assets/src/js/main.js
@@ -1,2 +1,47 @@
-const timer = document.querySelector('.container');
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.reiniciar');
 
+let segundos = 0;
+let timer;
+
+function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    }
+    );
+}
+
+function iniciarRelogio() {
+    timer = setInterval(function() {
+        segundos++
+        relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+}
+
+document.addEventListener('click', function(e) {
+    const elemento = e.target;
+
+    if(elemento.classList.contains('iniciar')) {
+        relogio.classList.remove('pausado');
+
+        clearInterval(timer);
+        iniciarRelogio()
+    } 
+    if(elemento.classList.contains('pausar')) {
+        relogio.classList.add('pausado');
+
+        clearInterval(timer);
+    } 
+    if(elemento.classList.contains('reiniciar')) {
+        relogio.classList.remove('pausado');
+
+        clearInterval(timer)
+        relogio.innerHTML = '00:00:00'
+        segundos = 0;
+    } 
+})
---
+++ Secao-3/aula47/assets/src/js/main.js
@@ -0,0 +1,64 @@
+const taskButton = document.querySelector('.add-task-btn');
+const newTaskInput = document.querySelector('.new-task-input');
+const taskList = document.querySelector('.task')
+
+function createLi() {
+     const li = document.createElement('li');
+
+     return li;
+}
+
+function createButtonDelete(li) {
+     li.innerText += ' ';
+
+     const button = document.createElement('button');
+
+     button.innerText = 'Apagar';
+
+     button.setAttribute('class', 'apagar')
+     li.appendChild(button);
+}
+
+function createTask(textInput) {
+     const li = createLi()
+
+     li.innerText = textInput;
+
+     taskList.appendChild(li);
+
+     clearInput();
+     createButtonDelete(li);
+     saveTask();
+}
+
+function clearInput() {
+     newTaskInput.value = '';
+
+     newTaskInput.focus();
+}
+
+taskButton.addEventListener('click', function () {
+     if (!newTaskInput.value) return;
+
+
+     createTask(newTaskInput.value);
+})
+
+document.addEventListener('click', function(e) {
+     const elemento = e.target;
+
+     if(elemento.classList.contains('apagar')){
+          elemento.parentElement.remove();
+     }
+})
+
+function saveTask() {
+     const liTask = taskList.querySelectorAll('li')
+     const listaDeTarefas = [];
+
+     for (let tarefa of liTask){
+          listaDeTarefas.push(tarefa.innerText.replace('Apagar', '').trim());
+     }
+     const tarefasJson = JSON.stringify(listaDeTarefas);
+     localStorage.setItem('tarefas', tarefasJson);
+}
